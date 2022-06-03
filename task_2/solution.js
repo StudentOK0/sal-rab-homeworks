3e0d853beba181e676ec371354a04bc4bf410a8f
olga koval <Kovolg2025@mail.ru>
2022-06-03 11:12:25 +0300

    task_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,8 +15,15 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+        else ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+        shippingSum = shippingPrice;
+    }
+    
+        // Конец решения задания №2.1.
 
     return shippingSum;
 }
@@ -33,6 +40,13 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+    let discountSum;
+    if (productsSum >= discountMinSum) {
+        (discountSum = discountSum / 100 * discountPart);
+    }
+    else {
+        (discountSum = 0);
+    }
 
     // Конец решения задания №2.2.
 
@@ -49,15 +63,23 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
-
+    let totalSum = (productsSum - discountSum) + shippingSum;
+    
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
+    let freeShipping;
+    if (shippingSum = 0) {
+       (freeShipping = true);
+}
+    else {
+    (freeShipping = false);
+} 
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
